5db655081dcaa74c7d1acec2237655c7563ca952
Cheruzir <153986908+Cheruzir@users.noreply.github.com>
2024-09-21 16:44:38 +0300

    Update code copy.js

--- Game_js/arcanoid/code copy.js
+++ Game_js/arcanoid/code copy.js
@@ -3,8 +3,17 @@ let ctx = canvas.getContext("2d")
 x = canvas.wight/2
 y= canvas.height-40
 
+let dx = 0.5
+let dy = -0.5
+ function mova() {
 ctx.beginPath();
 ctx.arc(x, y, 10, 0, Math.PI * 2);
 ctx.fillStyle = "#0095DD";
 ctx.fill();
+ctx.closePath();
+x+=dx
+y+=dy
+}
+ctx.beginPath();
+setInterval(mova,5)
 ctx.closePath();
